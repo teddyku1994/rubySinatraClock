fa1cc614ba65a94f403d11d16ceae4f141c30b7a
Teddy <teddyku1994@gmail.com>
2019-11-21 17:18:17 +0800

    update timezone to GMT + 8

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require "sinatra"
 set :bind, '0.0.0.0'
 
 get "/" do
-  time = Time.new
+  offset = +8
+  time = Time.now + offset * 3600
   @timeNow = time.strftime("%Y-%m-%d %I:%M:%S %p")
   erb :index
 end
